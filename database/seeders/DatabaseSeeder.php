c1818950ba86855586df22a86bcd6995e2a109d5
emmaru <littleoduozu@gmail.com>
2024-04-14 22:28:41 +0900

    Seederの導入

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Image;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Product::factory(4)
+            ->hasVariants(5)
+            ->has(Image::factory(3)
+                ->sequence(fn (Sequence $sequence) => ['featured' => $sequence->index % 3 === 0]))
+            ->create();
     }
 }
