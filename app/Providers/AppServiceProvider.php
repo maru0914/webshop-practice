04d2738bffbb8a03a5ef828fc242969abe5a44e8
emmaru <littleoduozu@gmail.com>
2024-04-23 08:43:47 +0900

    Cashierの導入

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,17 @@
 
 namespace App\Providers;
 
+use App\Actions\Webshop\MigrateSessionCart;
+use App\Factories\CartFactory;
+use App\Models\Cart;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Cashier\Cashier;
+use Laravel\Fortify\Fortify;
 use Money\Currencies\ISOCurrencies;
 use Money\Formatter\IntlMoneyFormatter;
 use Money\Money;
@@ -26,6 +34,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
+        Cashier::calculateTaxes();
+
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::where('email', $request->email)->first();
+
+            if ($user &&
+                Hash::check($request->password, $user->password)) {
+                (new MigrateSessionCart)->migrate(CartFactory::make(), $user?->cart ?: $user->cart()->create());
+                return $user;
+            }
+        });
 
         Blade::stringable(function (Money $money) {
             $currencies = new ISOCurrencies();
