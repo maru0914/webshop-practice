cac9be157636e1543e09cfe977ca36392c7f7ef3
emmaru <littleoduozu@gmail.com>
2024-04-27 11:45:47 +0900

    支払い時に配送先住所を収集する

--- app/Actions/Webshop/CreateStripeCheckoutSession.php
+++ app/Actions/Webshop/CreateStripeCheckoutSession.php
@@ -14,7 +14,17 @@ class CreateStripeCheckoutSession
         return $cart->user
             ->allowPromotionCodes()
             ->checkout(
-            $this->formatCartItems($cart->items)
+            $this->formatCartItems($cart->items),
+                [
+                    'customer_update' => [
+                        'shipping' => 'auto',
+                    ],
+                    'shipping_address_collection' => [
+                        'allowed_countries' => [
+                            'JP'
+                        ]
+                    ]
+                ]
         );
     }
 
